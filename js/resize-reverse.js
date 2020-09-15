9a9cc7239255306d4469d32e57a16110586e7d2f
Mike Tschudi <mtschudi@esri.com>
2020-09-15 06:01:00 -0700

    Updated plugin trigger

--- js/resize-reverse.js
+++ js/resize-reverse.js
@@ -15,7 +15,7 @@
 | See the License for the specific language governing permissions and
 | limitations under the License.
 */
-$(window).load(function () {
+$.when( $.ready ).then(function () {
     $.ui.plugin.add("resizable", "alsoResizeReverse", {
 
         start: function () {
